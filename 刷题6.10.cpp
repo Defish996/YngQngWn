bd032865332bba90c359e53269af5419321d8de7
wyq <3133866171@qq.com>
2024-06-10 20:36:09 +0800

    设计类

--- 刷题6.10.cpp
+++ 刷题6.10.cpp
@@ -26,3 +26,50 @@ public:
         return ans;
     }
 };
+
+//380. O(n) 时间插入、删除和获取随机元素
+class RandomizedSet {
+public:
+    RandomizedSet() {
+
+    }
+    
+    bool insert(int val) {
+        if(find(num.begin(), num.end(), val) != num.end())//找到啦,存在
+        {
+            return false;
+        }
+        else
+        {
+            num.push_back(val);
+            return true;
+        }
+    }
+    
+    bool remove(int val) {
+        auto e = find(num.begin(), num.end(), val);
+        if(e != num.end())
+        {
+            num.erase(e);
+            return true;
+        }
+        return false;
+    }
+    
+    int getRandom() {
+        int tmp = rand() % (int)num.size();
+        return num[tmp];
+
+    }
+private:
+    vector<int> num; 
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
+
