770fd5b1f5ba639f584cd15b07f34278e0cb5252
wyq <3133866171@qq.com>
2024-08-13 22:50:24 +0800

    leetcode 125. 验证回文串 双指针

--- 刷题8.13.cpp
+++ 刷题8.13.cpp
@@ -14,3 +14,34 @@ public:
         return true;
     }
 };
+
+// leetcode 125. 验证回文串 双指针
+
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = s.size() - 1;
+        while(left < right)
+        {
+            while(left < right && !isalnum(s[left])) // 不是字母或数字,左右两边都跳过
+            {
+                ++left;
+            }
+            while(left < right && !isalnum(s[right]))
+            {
+                --right;
+            }
+            if(left < right) // 每次将指针移到下一个字母字符或数字字符，再判断这两个指针指向的字符是否相同
+            {
+                if(tolower(s[left]) != tolower(s[right]))
+                {
+                    return false;
+                }
+            }
+            ++left;
+            --right;   
+        }
+        return true;
+    }
+};
