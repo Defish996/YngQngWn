76bfae341c82c9979d40775ad13026009e1a6966
wyq <3133866171@qq.com>
2024-08-25 22:39:14 +0800

    219. 存在重复元素 II + 优化版本

--- 刷题8.25.cpp
+++ 刷题8.25.cpp
@@ -16,3 +16,53 @@ public:
         return false;
     }
 };
+
+// 219. 存在重复元素 II
+
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int x) {
+        unordered_map<int, vector<int>> map;
+        int n = nums.size();
+        for(int i = 0; i < n; i++)
+        {
+            map[nums[i]].push_back(i);
+            if(map[nums[i]].size() >= 2)
+            {
+                for(int j = 0; j < map[nums[i]].size(); j++)
+                {
+                    for(int k = j + 1; k < map[nums[i]].size(); k++)
+                    {
+                        if(abs(map[nums[i]][j] - map[nums[i]][k]) <= x)
+                        {   
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
+
+// 优化版本
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> map;
+        int n = nums.size();
+        for(int i = 0; i < n; i++)
+        {
+            if(map.count(nums[i]))
+            {
+                if(i - map[nums[i]] <= k)
+                {
+                    return true;
+                }
+            }
+            map[nums[i]] = i;
+        }
+        return false;
+    }
+};
