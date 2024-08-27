cef5a0805956adf6ceadb9d41b306af33fe2085a
wyq <3133866171@qq.com>
2024-08-27 14:43:35 +0800

    二叉树节点个数+各位相加(同余定理的应用)

--- 刷题8.27.cpp
+++ 刷题8.27.cpp
@@ -23,3 +23,56 @@ public:
         return true;
     }
 };
+
+// 二叉树中的节点个数 
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root == nullptr) return 0;
+        return countNodes(root->left) + countNodes(root->right) + 1;// 1为当前节点
+    }
+};
+
+// leetcode 258. 各位相加
+// 模拟法
+class Solution {
+public:
+    int addDigits(int num) {
+        int ans = 0;
+    all:
+        while(num > 0)
+        {
+            ans += num % 10;
+            num /= 10;
+        }
+        if(ans > 9)
+        {
+            num = ans;
+            ans = 0;
+            goto all; 
+        }
+        return ans;
+    }
+};
+// 数学规律 同余定理的应用
+class Solution {
+public:
+    int addDigits(int num) {
+        return (num - 1) % 9 + 1;
+    }
+};
+
+
+ 
