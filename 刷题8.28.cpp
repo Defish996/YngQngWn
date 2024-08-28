b9a7ca54f3f2353aa5fc81d58cd3ab54a3b54b8f
wyq <3133866171@qq.com>
2024-08-28 13:47:04 +0800

    278. 第一个错误的版本  二分+268. 丢失的数字 hash+计数

--- 刷题8.28.cpp
+++ 刷题8.28.cpp
@@ -18,3 +18,52 @@ public:
         return n == 1;
     }
 };
+
+//268. 丢失的数字 hash+计数
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> hash(n + 1);
+        for(auto &e : nums)
+        {
+            hash[e] = 1;
+        }
+        for(int i = 0; i <= n; i++)
+        {
+            if(hash[i] == 0)
+            {
+                return i; 
+            }
+        }
+        return 0;
+    }
+};
+
+// 278. 第一个错误的版本  二分
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while(left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if(isBadVersion(mid))
+            {
+                right = mid - 1;
+            }
+            else if(!isBadVersion(mid))
+            {
+                left = mid + 1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return left;
+    }
+};
