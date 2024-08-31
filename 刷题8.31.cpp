0c97eff47ae9ac133f86c7261366cfb726db9d04
wyq <3133866171@qq.com>
2024-08-31 12:23:12 +0800

    同构字符串 hash双向映射

--- 刷题8.31.cpp
+++ 刷题8.31.cpp
@@ -22,3 +22,26 @@ public:
         return true;
     }
 };
+
+// 205. 同构字符串 hash双向映射
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        // 本题是关于char与char之间的映射关系, 所以在添加之前看是否已添加映射, 若存在, 就需要判断当前的字符对应的映射是否满足双向映射, 否则进行添加
+        unordered_map<char, char> map1;
+        unordered_map<char, char> map2;
+        int n = s.size();
+        for(int i = 0; i < n; i++)
+        {
+            char a = s[i], b = t[i];
+            if(map1.find(a) != map1.end() && map1[a] != b || map2.find(b) != map2.end() && map2[b] != a)// s向t映射的检查, t向s也需要检查
+            {
+                return false;
+            }
+            map1[a] = b;
+            map2[b] = a;
+        }
+        return true;
+    }
+};
