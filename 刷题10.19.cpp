2e9f04274bb84296a88a10f8d26ec8fe021cd6ad
wyq <3133866171@qq.com>
2024-10-19 23:36:10 +0800

    3192. 使二进制数组全部等于 1 的最少操作次数 II 利用奇偶性替换位运算

--- 刷题10.19.cpp
+++ 刷题10.19.cpp
@@ -31,3 +31,25 @@ int main()
 
 	return 0;
 }
+
+// 3192. 使二进制数组全部等于 1 的最少操作次数 II 利用奇偶性替换位运算
+
+class Solution {
+public:
+    int minOperations(vector<int>& vec) {
+	int ans = 0;
+	for (auto& x : vec)
+	{
+		// if (x == 0 && 0 == ans % 2) // ans % 2 , 若ans为偶数, 则取模之后为0, 否则为1, 偶数次则不进行操作
+		// 简化之后就是 值为0, 且为偶数次操作时(偶数等于没操作, 当成是第一次操作)
+		// 这是第一种, 另一种是:
+		// 当x为1时, 且ans % 2为1(奇数)时, 此时为需要变化的情况, 说明是奇次的变化使得原先的1变成了0, 所以需要进行变化
+		// if(x == 1 && 1 == ans & 2)
+		if(x == ans % 2)
+		{
+			++ans;
+		}
+	}
+	return ans;
+    }
+};
