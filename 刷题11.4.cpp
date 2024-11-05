b4ec4646e8b88413a880a47f24a42058a609f8bf
wyq <3133866171@qq.com>
2024-11-05 08:50:33 +0800

    昨日一题解法二 双指针+数学

--- 刷题11.4.cpp
+++ 刷题11.4.cpp
@@ -15,3 +15,22 @@ public:
         return false;
     }
 };
+
+// 方法二 双指针+数学
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        // 假设a = 0, b = 根号c, 则一定有a*a + b*b = c; // 这样的假设不失一般性的数据的a*a + b*b = c * c的判断
+        // 那么这样的假设会使满足题意的结果变为a*a + b*b = c, 当a*a + b*b < c时, 需要扩大化, 反之则要缩小, 看代码
+        long a = 0;
+        long b = (int)sqrt(c);// 万一开方是一个小数, 转为整数不影响对结果的判断
+        while(a <= b)
+        {
+            long sum = a * a + b * b;
+            if(sum == c) return true;
+            else if(sum > c) --b;
+            else ++a;
+        }
+        return false;
+    }
+};
